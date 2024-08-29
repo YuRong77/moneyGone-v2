7b6a59896affbb2c7351700a1002307b8ca691d1
YuRong <gcobc50335@gmail.com>
2024-08-30 00:17:57 +0800

    Updates

---
+++ assets/priceFormat-DV0X-7kS.js
@@ -0,0 +1,1 @@
+const t=r=>r?new Intl.NumberFormat("zh-TW",{style:"decimal",currency:"TWD",minimumFractionDigits:0,maximumFractionDigits:0}).format(r):0;export{t as p};
--- assets/validator-CzJSLmSr.js
+++
@@ -1,1 +0,0 @@
-import{bK as i}from"./index-H5iqYx0H.js";const n=o=>["",...i].includes(o);export{n as i};
