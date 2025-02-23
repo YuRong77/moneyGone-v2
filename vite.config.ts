1ca4963955ae11910a741da3a5c850dd01cada71
YuRong <gcobc50335@gmail.com>
2025-02-23 23:10:19 +0800

    fix: adjust vite config

--- vite.config.ts
+++ vite.config.ts
@@ -59,9 +59,9 @@ export default defineConfig({
           // if (id.includes('node_modules')) {
           //   return 'vendor'
           // }
-          if (id.includes('/src/views/')) {
-            return 'views'
-          }
+          // if (id.includes('/src/views/')) {
+          //   return 'views'
+          // }
           if (id.includes('src/apis/')) {
             return 'api-chunk'
           }
